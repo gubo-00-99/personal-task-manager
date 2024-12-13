4d6fed0c4e7c22c2fde9515cb8880a53df2de453
gubo <gubo.00.99@gmail.com>
2024-12-13 17:49:56 +0900

    Add Spring Boot project to backend folder

---
+++ backend/personal-task-manager/src/test/java/com/gubo/personal_task_manager/PersonalTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gubo.personal_task_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
